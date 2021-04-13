8eb55568bbb6a14a423ccc4caae31a31e43a12ef
super <superlfx@163.com>
2021-04-13 23:03:54 +0800

    睡前提交

---
+++ S_source/src/superlfx/main/spring.java
@@ -0,0 +1,18 @@
+package superlfx.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class spring {
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("");
+        applicationContext.getBean("");
+
+
+
+    }
+
+
+
+}
